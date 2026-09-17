14b123b5822727ace2d86fc233cd268df003242d
RasmusDB <rab00012@laurea.fi>
2026-09-17 12:55:05 +0300

    WS02 Complete

--- WS02_JavaScript_Basics/js/script.js
+++ WS02_JavaScript_Basics/js/script.js
@@ -6,35 +6,57 @@ console.log("Hello, World!");
 
 // Store text values in variables
 
-const userName = "Juho";
-const favoriteAnimal = "Blue Elephant";
+let userName = "Rasmus";
+let age = 26;
+const favoriteAnimal = "Maisa koira";
 
 
 // Print the variables to the console
+
 console.log("My name is " + userName);
 console.log("My favorite animal is " + favoriteAnimal);
 // STEP 3: User interaction
 // Display a pop-up message
-alert("tervertetuloa hieoulle nettisivylleni!");
+
+alert("Tervetuloa nettisivulle!");
 
 // Ask the user for their name
 
 const visitorName = prompt("Mikä on nimesi?");
 
 // Print the user's answer
-console.log(visitorName);
 
-console.log("Terve " + visitorName + "! Tervetuloa javasciptimn psariin!");
+console.log(visitorName);
+console.log(age);
+
+console.log("Terve " + visitorName + "! Tervetuloa javasciptin pariin!");
 // Ask the user for their favorite animal
 
 const visitorFavoriteAnimal = prompt("Mikä on lempieläimesi?");
 
-// Create a sentence using both answers
+console.log("Hello, " + visitorName + "!");
 
-console.log("Hello, " + visitorName + "! Your favorite animal is " + visitorFavoriteAnimal + ".");
+if (visitorFavoriteAnimal === "Blue Elephant") {
+  console.log("Great choice! That's my favorite animal too!");
+} else {
+  console.log("Nice! Your favorite animal is " + visitorFavoriteAnimal + ".");
+}
+
+// Create a sentence using both answers (check above)
 
 // ⭐⭐ BONUS Ask the user for their favorite animal.If their favorite animal is "Blue Elephant", print: Great choice! That's my favorite animal too!
 // ⭐⭐ BONUS Otherwise, print: Nice! Your favorite animal is [animal]. Can you figure out how to do this using if / else?
 
-// const visitorFavoriteAnimal = prompt("What is your favorite animal?");
+const vAge = prompt("How old are you?");
 
+if (vAge < 18) {
+  console.log("You are under 18.");
+} else {
+  console.log("You are an adult.");
+}
+
+const messageButton = document.getElementById("messageButton");
+
+messageButton.addEventListener("click", function () {
+  alert("JavaScript works!");
+});
